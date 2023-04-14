3474773aa92554c3f0fbd6ca19959edaa82c81bc
Ajay Agrawal <94609372+ajayagrawalgit@users.noreply.github.com>
2023-04-15 00:23:21 +0530

    name correction

--- src/convertmyimages.sh
+++ src/convertmyimages.sh
@@ -5,4 +5,4 @@ destination_path=$2
 init_file_format=$3
 final_file_format=$4
 
-python3 /usr/local/lib/convert-my-images/src/main.py $source_path $destination_path $init_file_format $final_file_format
+python3 /usr/local/lib/convertmyimages/src/main.py $source_path $destination_path $init_file_format $final_file_format
